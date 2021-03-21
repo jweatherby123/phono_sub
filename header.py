aae41f143afefe27f3e7f22b2347f6682c879340
jweatherby123 <joseph.weatherby@dal.ca>
2021-03-20 21:58:47 -0300

    started phono_sub

---
+++ header.py
@@ -0,0 +1,21 @@
+def header(version):
+	print("       _                                    _   ")    
+	print(" _ __ | |__   ___  _ __   ___     ___ _   _| |__ ")
+	print("| '_ \| '_ \ / _ \| '_ \ / _ \   / __| | | | '_ \\") 
+	print("| |_) | | | | (_) | | | | (_) |  \__ \ |_| | |_) |")
+	print("| .__/|_| |_|\___/|_| |_|\___/___|___/\__,_|_.__/ ")
+	print("|_|                         |_____|               ")
+	print("Written by Joe Weatherby 2021")
+	print("A central way to submit phonon calculations")
+	print("Supported codes : QE[TBD] CRYSTAL17[TBD] DTFB+[TBD]")
+	print("")
+	print("Version = " + str(version))
+
+
+def things_im_working_on():
+	print("")
+	print("-----Things I'm currently working on.---")
+	print("Reading a CIF file.")
+	print("Interfacing with critic2")
+	print("Interfacing with DFTB+")
+	print("-----------------------------------------")
---
+++ main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3.8
+ 
+import header
+
+def main():
+	start_up(version)
+
+
+def start_up(version):
+	header.header(version)
+	header.things_im_working_on()
+
+
+version = 0.1
+
+if __name__ == "__main__":
+ main()
